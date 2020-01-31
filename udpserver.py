1a303b9c2c173a9724df0b861e7590f7ffaa54e5
Mpatidar059 <46708648+Mpatidar059@users.noreply.github.com>
2020-01-31 22:09:40 +0530

    Create udpserver.py

---
+++ udpserver.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# Script to run UDP Server on 127.0.0.1:8126
+# This mocks statsd server for testing
+
+import socket
+
+UDP_IP_ADDRESS = "127.0.0.1"
+UDP_PORT_NO = 8126
+
+serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
+
+while True:
+    data, addr = serverSock.recvfrom(1024)
+    print "Message: ", data
